956158b53a4cad5db2607c6cfdd99bdf90afdb56
OleksiiPro1 <ukr.top@ukr.net>
2026-04-07 09:39:21 +0200

    fix: remove frontend admin preview bar

--- src/app/(frontend)/[locale]/layout.tsx
+++ src/app/(frontend)/[locale]/layout.tsx
@@ -2,8 +2,6 @@ export const dynamic = 'force-dynamic'
 import React from 'react'
 import { Providers } from '@/providers'
 import { InitTheme } from '@/providers/Theme/InitTheme'
-import { AdminBar } from '@/components/AdminBar'
-import { draftMode } from 'next/headers'
 import { cn } from '@/utilities/ui'
 import { Montserrat } from 'next/font/google'
 import '../globals.css'
@@ -16,13 +14,10 @@ const montserrat = Montserrat({
 })
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
-  const { isEnabled } = await draftMode()
-
   return (
     <div className={cn(montserrat.variable, 'antialiased font-sans')}>
       <InitTheme />
       <Providers>
-        {isEnabled && <AdminBar adminBarProps={{ preview: isEnabled }} />}
         <main>{children}</main>
       </Providers>
     </div>
